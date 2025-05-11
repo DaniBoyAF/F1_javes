11b75e50ee4586e0452e29a05108572cea3a9db8
Daniel <moongames12345@gmail.com>
2025-05-11 15:03:45 -0300

    urls completas

--- FF/urls.py
+++ FF/urls.py
@@ -1,16 +1,18 @@
-from django.urls import path
-from FF.views import views
+from django.urls import path ,include
+from django.contrib import admin
+from FF.views import InicioView, TelemetriaView,SessionData, CarStatus, LapData,Penaty, Usuario, Amizade
 from FF.udp_listener import udp_server
 
+
 urlpatterns = [
-    path('', views.InicioView.as_view(), name='index'),
-    path('api/telemetria/', views.telemetria_view, name='telemetria'),
-    path('api/sessao/', views.session_data_view, name='sessao'),
-    path('api/status-carro/', views.car_status_view, name='status_carro'),
-    path('api/voltas/', views.lap_data_view, name='voltas'),
-    path('api/punicoes/', views.penaty_view, name='punicoes'),
-    path('amizade/enviar<int:destinatario_id>/', views.enviar_solicitacao.as_view(), name='enviar_solicitacao'),
-    path('amizade/aceitar<int:amizade_id>/', views.aceitar_solicitacao.as_view(), name='aceitar_solicitacao'),
-    path('amizade/recusar<int:amizade_id>/', views.recusar_solicitacao.as_view(), name='recusar_solicitacao'),
+    path('', InicioView.as_view(), name='index'),
+    path('api/telemetria/', TelemetriaView.as_view(), name='telemetria'),
+    path('api/sessao/', SessionData.as_view(), name='sessao'),
+    path('api/status-carro/', CarStatus.as_view(), name='status_carro'),
+    path('api/voltas/', LapData.as_view(), name='voltas'),
+    path('api/punicoes/', Penaty.as_view(), name='punicoes'),
+    path('amizade/enviar<int:destinatario_id>/', Amizade.as_view(), name='enviar_solicitacao'),
+    path('amizade/aceitar<int:amizade_id>/', Amizade.as_view(), name='aceitar_solicitacao'),
+    path('amizade/recusar<int:amizade_id>/', Amizade.as_view(), name='recusar_solicitacao'),
 
 ]
